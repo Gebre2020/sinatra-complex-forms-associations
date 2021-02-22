9b33cdd674e99dbb5f2cad2a15ee5baf16ed0aea
Gebremeskel <gebre20122@gmail.com>
2021-02-22 09:22:14 -0600

    associated objects to one another

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,13 +6,23 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    # binding.pry
+     @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    else
+      @pet.owner = Owner.find_by_id(params[:pet][:owner_id])
+    end
+    @pet.save
 
     redirect to "pets/#{@pet.id}"
   end
+
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
@@ -20,7 +30,7 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-
+  # post '/pets/:id' do
     redirect to "pets/#{@pet.id}"
   end
 end
